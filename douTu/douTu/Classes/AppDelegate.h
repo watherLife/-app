d889fb36074caa2c9b7ca93ff61630e1c46396b3
赵江风 <13483776522@163.com>
2016-12-01 15:34:47 +0800

    架构项目

---
+++ douTu/douTu/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  douTu
+//
+//  Created by wyzc04 on 16/12/1.
+//  Copyright © 2016年 wyzc04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ douTu/douTu/Classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  douTu
+//
+//  Created by wyzc04 on 16/12/1.
+//  Copyright © 2016年 wyzc04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
